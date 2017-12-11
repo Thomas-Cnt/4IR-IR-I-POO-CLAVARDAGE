f1e86ed20c39274cb14fef40692027135157cb8b
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-12-11 18:39:22 +0100

    -- refactor with Application

--- src/main/java/com/thomascantie/insa/Launcher.java
+++ src/main/java/com/thomascantie/insa/Launcher.java
@@ -1,56 +1,12 @@
 package com.thomascantie.insa;
 
-import com.thomascantie.insa.controler.ApplicationListener;
-import com.thomascantie.insa.model.core.ChatManager;
-import com.thomascantie.insa.model.core.ConnectionsManager;
-import com.thomascantie.insa.model.core.User;
-import com.thomascantie.insa.model.network.util.LocalNetworkIPAddress;
-import com.thomascantie.insa.model.network.util.PortNumber;
-import com.thomascantie.insa.view.InputPseudo;
-import com.thomascantie.insa.view.ViewConnections;
-
-import javax.swing.*;
-
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.net.InetAddress;
-
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import com.thomascantie.insa.view.Application;
 
 public class Launcher {
 
 	public static void main(String[] args) throws Exception {
 
-		User usr = new User(args[0]);
-
-		String localIPAddress = LocalNetworkIPAddress.getLocalIPAddress();
-		int listeningPortNumber = PortNumber.generate();
-
-		ViewConnections view = new ViewConnections(usr, localIPAddress, listeningPortNumber);
-
-		ConnectionsManager.getInstance().setUser(usr);
-		ConnectionsManager.getInstance().setViewConnections(view);
-		ConnectionsManager.getInstance().listeningNotifications();
-		ConnectionsManager.getInstance().notifyConnectionOn();
-
-		ChatManager.getInstance().setPseudo(args[0]);
-		ChatManager.getInstance().setLocalIPAddress(InetAddress.getByName(localIPAddress));
-		ChatManager.getInstance().setLocalPortNumber(listeningPortNumber);
-		ChatManager.getInstance().listeningIncommingMessages();
-
-		JFrame frame = new JFrame("Application de clavardage - " + usr);
-
-		frame.setContentPane(view);
-		//frame.setContentPane(new InputPseudo());
-
-		frame.pack();
-		frame.setResizable(true);
-		frame.setSize(600, 300);
-		//frame.setSize(400, 70);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frame.addWindowListener(new ApplicationListener());
-		frame.setVisible(true);
+		Application.getInstance().start();
 
 	}
 
